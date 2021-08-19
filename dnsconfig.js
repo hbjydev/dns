2f696bd83b54916d05a3b14ec33345acd471ee33
Hayden Young <hi@hbjy.dev>
2021-08-19 23:18:26 +0100

    fix: add period to each cname and mx record

--- dnsconfig.js
+++ dnsconfig.js
@@ -33,22 +33,22 @@ D('hbjy.dev', noreg, DnsProvider(cloudflare),
   cf_proxy_default_off,
 
   A('@', ips['vercel']),
-  CNAME('www', 'hbjy.dev'),
+  CNAME('www', 'hbjy.dev.'),
 
-  CNAME('_domainconnect', 'connect.domains.google.com'),
-  CNAME('openpgpkey', 'wkd.keys.openpgp.org'),
-  CNAME('s3', 's3.hbjy.dev.s3.fr-par.scw.cloud', cf_proxy_on),
-  CNAME('scripts', 'hbjydev.github.io'),
+  CNAME('_domainconnect', 'connect.domains.google.com.'),
+  CNAME('openpgpkey', 'wkd.keys.openpgp.org.'),
+  CNAME('s3', 's3.hbjy.dev.s3.fr-par.scw.cloud.', cf_proxy_on),
+  CNAME('scripts', 'hbjydev.github.io.'),
 
   TXT('_scaleway-challenge', '59ef7059-fccf-4ff3-8230-60dbac852afc'),
   TXT('@', 'keybase-site-verification=Wr2DT9CD4z8RQ2sLSVjdLtezue-d8BVio7VsRi52RUQ'),
 
   // ---- email config ----
-  MX('@', 1, 'aspmx.l.google.com'),
-  MX('@', 5, 'alt1.aspmx.l.google.com'),
-  MX('@', 5, 'alt2.aspmx.l.google.com'),
-  MX('@', 10, 'alt3.aspmx.l.google.com'),
-  MX('@', 10, 'alt4.aspmx.l.google.com')
+  MX('@', 1, 'aspmx.l.google.com.'),
+  MX('@', 5, 'alt1.aspmx.l.google.com.'),
+  MX('@', 5, 'alt2.aspmx.l.google.com.'),
+  MX('@', 10, 'alt3.aspmx.l.google.com.'),
+  MX('@', 10, 'alt4.aspmx.l.google.com.')
   // ---- end email config ----
 );
 
