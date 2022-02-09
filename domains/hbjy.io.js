0d096a6c44dee9ad5060bf18e64fd3b0491dbe44
Hayden <hi@hbjy.dev>
2022-02-09 19:15:30 +0000

    Update hbjy.io.js

--- domains/hbjy.io.js
+++ domains/hbjy.io.js
@@ -13,6 +13,8 @@ D('hbjy.io', noreg, DnsProvider(cloudflare),
 
   A('cdg-dat-postgres01.scw', ips['postgres01']),
 
-  TXT('@', 'keybase-site-verification=oGR6lPFIq4xje-3OMIr0UNvaF5Di5oSJzePC2_61kho')
+  TXT('@', 'keybase-site-verification=oGR6lPFIq4xje-3OMIr0UNvaF5Di5oSJzePC2_61kho'),
+  
+  CNAME('cdg-dev-s3', 'cdg-dev-s3.hbjy.io.s3.fr-par.scw.cloud')
 );
 
