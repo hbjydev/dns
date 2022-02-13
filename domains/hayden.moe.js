1692afd28bec3ba3dd967c8f74ba9e03967a1c99
Hayden <hi@hbjy.dev>
2022-02-13 23:01:22 +0000

    Update hayden.moe.js

--- domains/hayden.moe.js
+++ domains/hayden.moe.js
@@ -1,3 +1,9 @@
 D('hayden.moe', noreg, DnsProvider(cloudflare),
-  A('@', ips['vercel'])
+  A('@', ips['vercel']),
+  
+  MX('@', 10, 'mx01.mail.icloud.com.'),
+  MX('@', 10, 'mx02.mail.icloud.com.'),
+  TXT('@', 'v=spf1 include:icloud.com ~all'),
+  TXT('@', 'apple-domain=xZRvL6gLtIuJmYqG')
+  CNAME('sig1._domainkey', 'sig1.dkim.hayden.moe.at.icloudmailadmin.com.')
 );
